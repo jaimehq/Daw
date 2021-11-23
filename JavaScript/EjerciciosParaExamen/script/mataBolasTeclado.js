aaca4ffd86fadd8eed265fa6397b6d6b1a9b0106
jaimehq <92947496+jaimehq@users.noreply.github.com>
2021-11-23 13:31:42 +0100

    errorers

---
+++ JavaScript/EjerciciosParaExamen/script/mataBolasTeclado.js
@@ -0,0 +1,140 @@
+btnMouse.onclick = guardarNombre;
+btnTeclado.onclick = guardarNombreTeclado;
+document.addEventListener("keydown", moverBarra)
+
+let bola = document.querySelector("img");
+let intervalo,intervaloCaer;
+let usuario;
+let puntuacion;
+
+function moverBarra(evento) {
+    //debugger
+    let x = barra.offsetLeft;
+    if( bola.offsetTop>=barra.offsetTop && bola.offsetLeft>=barra.offsetLeft-10 && bola.offsetLeft<=barra.offsetLeft+10){
+        clearInterval(intervalo)
+        clearInterval(intervaloCaer);
+        bola.style.visibility="hidden";
+        sumarPuntos();
+    }
+    switch (true) {
+        case (evento.keyCode === 37):
+            if(x<window.innerWidth){
+            x -= 5
+            barra.style.left = `${(x)}px`}
+            break
+        case (evento.keyCode === 39):
+            if(x<window.innerWidth){
+            x += 5
+            barra.style.left = `${(x)}px`}
+            break
+        
+    }
+}
+function guardarNombreTeclado(evento) {
+    barra.style.visibility = "visible"
+    barra.style.top = `${(window.innerHeight - 80)}px`
+    barra.style.left = `${(window.innerWidth / 2)}px`
+    debugger
+    usuario = nombreI.value;
+    puntuacion = 0;
+    //escondemos el fomulario y hacemos visibles las tablas de puntos
+    score.style.visibility = "visible";
+    nombre.style.visibility = "hidden";
+    //mostramos la puntuacion actual que sera siempre 0
+    scoreActual.innerHTML = `Usuario=${usuario}<br>${puntuacion}`;
+    //y empezamos a crear bolas y mostraremos el ranking almacenado en local en caso de que lo haya
+    intervalo = setInterval(moverBarra, 5, evento)
+    creacionBolas();
+    mostrarRanking();
+    nombreI.value = "";
+
+}
+//lo primero que va a hacer la pagina es solicitar el nombre de usuario para empezar el juego
+function guardarNombre() {
+    bola.addEventListener("mousedown", sumarPuntos);
+    //iniciamos el usuario y la puntuacion
+    usuario = nombreI.value;
+    puntuacion = 0;
+    //escondemos el fomulario y hacemos visibles las tablas de puntos
+    score.style.visibility = "visible";
+    nombre.style.visibility = "hidden";
+    //mostramos la puntuacion actual que sera siempre 0
+    scoreActual.innerHTML = `Usuario=${usuario}<br>${puntuacion}`;
+    //y empezamos a crear bolas y mostraremos el ranking almacenado en local en caso de que lo haya
+    creacionBolas();
+    mostrarRanking();
+    nombreI.value = "";
+}
+//sumar puntos se activara cada click en la bola que nos sumara 10 puntos, actualizara el score, escondera la bola y creara otra
+function sumarPuntos() {
+    puntuacion += 10;
+    scoreActual.innerHTML = `Usuario=${usuario}<br>${puntuacion}`;
+    clearInterval(intervaloCaer)
+    bola.style.visibility = "hidden";
+    creacionBolas();
+}
+//con la funcion crear bola iremos creando bolas de forma random en en lo mas alto de la pagina
+
+function creacionBolas() {
+    //establecemos la altura
+    bola.style.top = "0px";
+    //lo hacemos visible
+    bola.style.visibility = "visible";
+    //y nos damos una posicion en el eje x aleatorio en funcion al ancho de pantalla del navegador
+    bola.style.left = `${Math.random() * window.innerWidth}px`;
+    //creamos el intervalo que hara caer la pelota
+    intervaloCaer = setInterval(function () {
+        let y = bola.offsetTop;
+        y += 5;
+        //iremos sumando 5 a su posicion cada unidad de tiempo que se asignara aleatoriamente
+        bola.style.top = `${y}px`;
+        //si la bola llega al final de la pantalla se eliminara el intervalo, y "reiniciara el juego"
+        if (bola.offsetTop >= window.innerHeight) {
+            clearInterval(intervaloCaer);//borra el intervalo
+            //muestra un mensaje de que la partida finalizo
+            scoreActual.innerHTML = `PARTIDA FINALIZADA<br> SU PUNTUACION ES:<br>${puntuacion} puntos`;
+            //vuelve a aparecer el formulario de nombre
+            nombre.style.visibility = "visible";
+            //guarda la puntuacion en local y actualiza el ranking
+            guardarPuntos();
+            mostrarRanking();
+        }
+    }, (Math.random() * 30) + 55)//el tiempo de intervalo tambien sera random dando un +5 por si el numero es muy bajo que podamos pararlo
+
+}
+//la funcion mostrar rancking busca en local la info y la muestra en su respectivo cuadro
+function mostrarRanking() {
+    let ranking = JSON.parse(localStorage.getItem("ranking"));
+
+    if (ranking != null) {
+        candenaTexto = "<table><tr><th>Posicion</th><th>Usuario</th><th>Puntuacion</th></tr>";
+        ranking.forEach(function (element, indice) {
+            candenaTexto += `<tr><td>${indice + 1}</td><td>${element.nombre}</td><td>${element.puntos}</td></tr>`;
+        });
+        candenaTexto += "</table>";
+        tablaScore.innerHTML = candenaTexto;
+    }
+}
+//guardar puntos se realizara una vez acabada la partida para actualizar el array local
+function guardarPuntos() {
+    //capturamos lo que haya en memoria
+    let ranking = JSON.parse(localStorage.getItem("ranking"));
+    //si no existe lo creamos
+    if (ranking === null) {
+        ranking = [];
+    }
+    // creamos un objeto con las propiedades nombre y puntos pa guardarlo en el array
+    let valor = {
+        nombre: usuario,
+        puntos: puntuacion
+    }
+    //lo introducimos dentro del array
+    ranking.push(valor);
+    //lo ordenamos por puntos
+    ranking.sort((a, b) => b.puntos - a.puntos);
+    //y si hay mas de 10 registros los borramos para que solo almacene los 10 primeros
+    ranking.splice(10, 1);
+    //y lo metemos en local
+    localStorage.setItem("ranking", JSON.stringify(ranking));
+
+}

835a17e89dd957a4c3e873824dd10dd9083c519b
jaimehq <jaimehq@alumnos.iesgalileo.es>
2021-11-24 17:38:38 +0100

    php

---
+++ PHP/EjercicioRepasoTema4/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Principal repaso</title>
+</head>
+<body>
+    <h1>Academia de Inglés</h1>
+    <p>Elige una opción:</p>
+    <ul>
+        <li><a href="horarios.php">Ver horarios clase</a></li>
+        <li><a href="matricularse.php">Matricularse curso</a></li>
+    </ul>
+</body>
+</html>
---
+++ PHP/EjercicioRepasoTema4/matricularse.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matricularse</title>
+</head>
+
+<body>
+    <h1>Matricula curso</h1>
+    <hr>
+    <table>
+        <tr>
+            <td>
+                Nombre:
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                DNI:
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Teléfono:
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Edad:
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Nivel:
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>

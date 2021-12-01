bc2a05ff51a63b47ed86bf51a9b8e25404c45115
jaimehq <jaimehq@alumnos.iesgalileo.es>
2021-12-01 20:52:51 +0100

    ejercicioProvincias

--- JavaScript/2Evaluacion/PrimeraClase/provincias.js
+++ JavaScript/2Evaluacion/PrimeraClase/provincias.js
@@ -66,19 +66,42 @@ comunidades.forEach(function (comunidad) {
 comunidadesS.addEventListener("change", mostrarProvincias);
 function mostrarProvincias() {
   comunidadesS = document.getElementById("comunidadesS");
-  let seleccionada = comunidadesS.selectedIndex
-  provincias.innerHTML=`<option value="">Seleccione una provincia</option>`;
-  listaProvincias[seleccionada-1].forEach(element => {
+  let seleccionada = comunidadesS.selectedIndex;
+  provincias.innerHTML = `<option value="">Seleccione una provincia</option>`;
+  listaProvincias[seleccionada - 1].forEach((element) => {
     provincias.innerHTML += `<option value="${element}">${element}</option>`;
   });
 }
 
-colores.addEventListener("change",cambioColor)
+colores.addEventListener("change", cambioColor);
 
-function cambioColor(){
-  redM.innerText=` Red: ${red.value}`;
-  greenM.innerText=` Green: ${green.value}`;
-  blueM.innerText=` Blue: ${blue.value}`;
- color.style.background=`rgb(${parseInt(red.value)},${parseInt(green.value)},${parseInt(blue.value)})`;
- 
+function cambioColor() {
+  redM.innerText = ` Red: ${red.value}`;
+  greenM.innerText = ` Green: ${green.value}`;
+  blueM.innerText = ` Blue: ${blue.value}`;
+  color.style.background = `rgb(${parseInt(red.value)},${parseInt(
+    green.value
+  )},${parseInt(blue.value)})`;
+  colorH.value = `#${cambioDecimal(red.value)}${cambioDecimal(
+    green.value
+  )}${cambioDecimal(blue.value)}`;
 }
+
+function cambioDecimal(numDec) {
+  let hexa;
+   if (numDec <= 16) {
+    hexa = `0${parseInt(numDec).toString(16)}`;
+    //debugger;
+  }else 
+  hexa = parseInt(numDec).toString(16);
+  return hexa;
+}
+let intervalo = setInterval(() => {
+ 
+  if (progreso.value <= progreso.max) {
+    progreso.value += 1;
+    progresoR.value = parseFloat(progresoR.value) + 1;
+  } else clearInterval(intervalo);
+}, 100);
+
+//mdm video de datalist

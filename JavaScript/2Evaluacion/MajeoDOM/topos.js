095b94c69f1329e936e8ef6aecd269b4f00d04db
jaimehq <92947496+jaimehq@users.noreply.github.com>
2021-12-22 13:22:12 +0100

    js

--- JavaScript/2Evaluacion/MajeoDOM/topos.js
+++ JavaScript/2Evaluacion/MajeoDOM/topos.js
@@ -7,33 +7,77 @@ En un documento crea una etiqueta <img src=”imagen.png”>.
 aciertos y ocultas la imagen.
 
 • Después de ocultarlas mostrar el porcentaje de aciertos en un alert. */
-let vidas=5;
-let nivel=1;
-let corazones=[];
-let corazon;
+let vidas = 15;
+let nivel = 1;
+let corazones = [];
+let topillos=[];
+let corazonPadre=document.createElement('img');
+corazonPadre.setAttribute('src','img/corazon.png')
+topoPadre.setAttribute('style','position:absolute')
+let toposVivos;
+let topoPadre=document.createElement('img');
+topoPadre.setAttribute('src','img/topo.png')
+topoPadre.setAttribute('style','position:absolute')
 //creamos las vidas
 //debugger
 //c=corazon.cloneNode(false);
-for(let i=0; i<vidas; i++){
-    corazon=vida.firstElementChild.cloneNode()
-    corazon.style.visibility='visible';
+for (let i = 0; i < vidas; i++) {
+    corazon = corazonPadre.cloneNode()
+    corazon.style.visibility = 'visible';
     vida.appendChild(corazon);
+    corazones.push(corazon)
 }
-document.addEventListener('keypress',empezar)
-function empezar(evento){
-    if(evento.keyCode===13){
-        let numero=5+(Math.random()*5)
-        let divPantalla=document.createElement('div');
+document.addEventListener('keypress', empezar)
+function empezar(evento) {
+    let texto = document.querySelector('h1');
+    pantalla.removeChild(texto);
+    if (evento.keyCode === 13) {
+        let numero = (4 + nivel) + (Math.floor(Math.random() * (4 + nivel)))
         //debugger
-        for(let i=0; i<numero; i++){
-            let topillo=pantalla.firstElementChild.cloneNode()
-            topillo.style.visibility='visible';
-            topillo.style.width='10%';
-            topillo.style.left = `${Math.random() * (window.innerWidth-topillo.clientWidth)}px`;
-            topillo.style.top = `${Math.random() * (window.innerHeight-topillo.clientHeight)}px`;
-            pantalla.appendChild(topillo);
+        for (let i = 0; i < numero; i++) {
+            let topillo = topoPadre.cloneNode()
+            let tamano = 150
+            topillo.style.width = `${tamano}px`;
+            //debugger
+            topillo.style.left = `${Math.random() * (window.innerWidth - tamano)}px`;
+            topillo.style.top = `${Math.random() * (window.innerHeight - tamano)}px`;
+            //debugger
+            pantalla.appendChild(topillo);            
         }
-        
-        //debugger
+        let seg = 0
+        temporizador = setInterval(function () {
+            seg++;
+            if (seg >= 3) {
+                let toposCreados = [...pantalla.children]
+                toposVivos = toposCreados.length;
+                toposCreados.forEach(topo => {
+                    topo.parentElement.removeChild(topo);
+                });
+                cambioNivel();
+                clearInterval(temporizador);
+            }
+        }, 1000)
+    }
+}
+function cambioNivel() {
+    vidas = vidas - toposVivos;
+    let h1 = document.createElement("h1");
+    if (vidas <= 0){
+        h1.innerText = `Estas Muerto pulsa Intro para comenzar`
+        nivel=1;
+        vidas=5;
+    }else{
+        nivel++;
+        h1.innerText = `Estas en el nivel ${nivel} pulsa intro para continuar`
+    }
+    pantalla.appendChild(h1);
+    corazones.forEach(cor => {
+        cor.parentElement.removeChild(cor)
+    });
+
+    for (let i = 0; i < vidas; i++) {
+        corazon = vida.firstElementChild.cloneNode()
+        corazon.style.visibility = 'visible';
+        vida.appendChild(corazon);
     }
 }

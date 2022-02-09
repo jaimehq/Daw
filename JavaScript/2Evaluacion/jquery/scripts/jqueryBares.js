d5c6d5f2ba3a3c1a92a76fb6daa5016e0f3e3809
jaimehq <jaimehq@alumnos.iesgalileo.es>
2022-02-09 21:03:28 +0100

    1

--- JavaScript/2Evaluacion/jquery/scripts/jqueryBares.js
+++ JavaScript/2Evaluacion/jquery/scripts/jqueryBares.js
@@ -1,4 +1,3 @@
-
 $.getJSON("https://analisis.datosabiertos.jcyl.es/api/records/1.0/search/?dataset=registro-de-turismo-de-castilla-y-leon&q=&rows=2090&facet=establecimiento&facet=provincia&facet=municipio&refine.provincia=Valladolid&refine.municipio=Valladolid&refine.establecimiento=Bares",
     function (respuesta) {
         $.each(respuesta.records, function (indice, bar) { 

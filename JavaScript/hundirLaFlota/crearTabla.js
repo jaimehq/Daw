9f7137a9714847c2d18edad2f394b1968ec62761
jaimehq <92947496+jaimehq@users.noreply.github.com>
2022-02-09 13:28:58 +0100

    tic

--- JavaScript/hundirLaFlota/crearTabla.js
+++ JavaScript/hundirLaFlota/crearTabla.js
@@ -1,3 +1,15 @@
+class Barco{
+  constructor(tamanoB,indiceY,indiceX,direccion){
+    this.tamanoB=tamanoB;
+    this.indiceX=indiceX;
+    this.indiceY=indiceY;
+    this.direccion=direccion;
+  }
+}
+let barcosColocados=[];
+let tamano,tamanoTabla;
+let direccion;
+//let x,y;
 function crearElemento(tipo, arrayAtributo, arrayTipo, texto) {
   let nodo = document.createElement(tipo);
   if (arrayAtributo != null && arrayTipo != null) {
@@ -21,11 +33,12 @@ function crearTabla(numCeldas) {
   } else if (numCeldas > 10) {
     numCeldas = 10;
   }
+  tamanoTabla=numCeldas;
   for (let i = 0; i < numCeldas + 1; i++) {
     if (i === 0) {
-      celdaTitulo = crearElemento("td");
+      celdaTitulo = crearElemento("td", ['class'], ['titulo']);
     } else {
-      celdaTitulo = crearElemento("td", [], [], i - 1);
+      celdaTitulo = crearElemento("td", ['class'], ['titulo'], i - 1);
     }
     fila.appendChild(celdaTitulo);
   }
@@ -34,7 +47,7 @@ function crearTabla(numCeldas) {
     fila = crearElemento("tr");
     for (let j = 0; j < numCeldas + 1; j++) {
       if (j === 0) {
-        celda = crearElemento("td", [], [], i);
+        celda = crearElemento("td", ['class'], ['titulo'], i);
       } else {
         celda = crearElemento("td", ["class"], [`${i}${j - 1}`]);
       }
@@ -47,17 +60,89 @@ function crearTabla(numCeldas) {
 
 aceptar.addEventListener("click", empezar);
 function empezar() {
-  let tamano = Number(inputTamano.value);
+  tamanoTabla = Number(inputTamano.value);
   $('#seleccionTamaño').remove();
-  debugger;
-  tablita.appendChild(crearTabla(tamano));
+  tablita.appendChild(crearTabla(tamanoTabla));
   document.body.appendChild(document.getElementById('plantilla').content)
-  $('#colocar').on('click',colocarBarcoG)
+  $('#colocar').on('click', predecirBarcoGestion)
 }
-function colocarBarcoG(){
-    let tamano= tamanoBarco.value;
-    if(tamano>4) tamano=4
-    else if(tamano<1) tamano=1;
-    let direccion=getElementsByName('direccionBarco')
-    debugger
+function predecirBarcoGestion() {
+  tamano = tamanoBarco.value;
+  direccion = '';
+  if (tamano > 4) tamano = '4'
+  else if (tamano < 1) tamano = '1';
+  let direcciones = document.getElementsByName('direccionBarco')
+  direcciones.forEach(dir => {
+    if (dir.checked) direccion = dir.value
+  })
+  $('td').on('mouseover', imprimirBarcoFantasma)
+  $('td').on('mouseleave', limpiarBarcosFantasma)
+  $('td').on('click', colocarBarcoGestion)
+}
+function colocarBarcoGestion(){
+  
+  if($(this).attr('class')!='titulo'){
+    let x=$(this).index();
+    let y=$(this).parent().index();
+    colocarBarco(tamano,y,x,direccion);
+  }
+}
+function limpiarBarcosFantasma() {
+  //debugger
+  $('td').filter('.fantasma').attr('class', '')
+}
+function imprimirBarcoFantasma() {  
+  if ($(this).attr('class') != 'titulo') {
+    let indice = $(this).index()
+    if (direccion == 'derecha') {
+      switch (tamano) {
+        case '4':
+          $(this).next().next().next().filter(':not(.colocado)').attr('class', 'fantasma')
+        case '3':
+          $(this).next().next().filter(':not(.colocado)').attr('class', 'fantasma')
+        case '2':
+          $(this).next().filter(':not(.colocado)').attr('class', 'fantasma')
+        case '1':
+          $(this).filter(':not(.colocado)').attr('class', 'fantasma')
+          break
+      }
+
+    }
+    if (direccion == 'abajo') {
+      switch (tamano) {
+        case '4':
+          $(this).parent().next().next().next().children().eq(indice).filter(':not(.colocado)').attr('class', 'fantasma')
+        case '3':
+          $(this).parent().next().next().children().eq(indice).filter(':not(.colocado)').attr('class', 'fantasma')
+        case '2':
+          $(this).parent().next().children().eq(indice).filter(':not(.colocado)').attr('class', 'fantasma')
+        case '1':
+          $(this).filter(':not(.colocado)').attr('class', 'fantasma')
+          break
+      }
+
+    }
+  }
+}
+function colocarBarco(tamanoBarco, fila, columna, direccion) {  
+  tamanoBarco=Number(tamanoBarco)
+  if(comprobarValidez(tamanoBarco, fila, columna, direccion)){
+    barcosColocados.push(new Barco(tamanoBarco, fila, columna, direccion))
+    $('td').filter('.fantasma').attr('class','colocado')
+  }else{
+    //hacer algo que avise
+  }
+}
+function comprobarValidez(tamanoBarco, fila, columna, direccion){
+  //debugger
+  let valido=false;
+  if(direccion==='derecha'){
+    if((tamanoTabla+1)>=(tamanoBarco+columna))
+      valido=true
+  }
+  else if(direccion==='abajo'){
+    if((tamanoTabla+1)>=(tamanoBarco+fila))
+      valido=true
+  }
+  return valido
 }
